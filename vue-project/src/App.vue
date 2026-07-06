830d91138297ba766f6c7fd02266c880263dce5b
Jay-Scripts <corneliogatbontonjr21@gmail.com>
2026-07-06 23:58:19 +0800

    added shadcn components

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,7 +1,38 @@
 <script setup lang="ts">
-// import TheWelcome from './components/TheWelcome.vue'
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import Test from "@/components/pages/Test.vue";
+
+// const products = [
+//   {
+//     id: 1,
+//     name: "Wireless Headphones",
+//     price: "$89.99",
+//     image: "https://placehold.co/400x300",
+//   },
+//   {
+//     id: 2,
+//     name: "Smart Watch",
+//     price: "$149.99",
+//     image: "https://placehold.co/400x300",
+//   },
+//   {
+//     id: 3,
+//     name: "Mechanical Keyboard",
+//     price: "$99.99",
+//     image: "https://placehold.co/400x300",
+//   },
+//   {
+//     id: 4,
+//     name: "Gaming Mouse",
+//     price: "$49.99",
+//     image: "https://placehold.co/400x300",
+//   },
+// ];
 </script>
 
-<template></template>
+<template>
+  <Test />
+</template>
 
 <style scoped></style>
---
+++ vue-project/src/components/pages/Test.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import { ref } from "vue";
+const name = ref("Cornelio");
+</script>
+<template>
+  <h1>{{ name }}</h1>
+</template>
