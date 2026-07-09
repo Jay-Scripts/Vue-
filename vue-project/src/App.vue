abb8cbacc1ea62e4733342b218f30d56ff91031a
Jay-Scripts <corneliogatbontonjr21@gmail.com>
2026-07-09 16:29:57 +0800

    v-bind : Bool a Attributes and Shorthands:

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -3,17 +3,21 @@ import { ref } from "vue";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import Test from "@/components/pages/Test.vue";
-
+const isBtnDisabled = ref(true);
 const author = ref("https://www.facebook.com/");
 </script>
 
 <template>
-  <Test />
-
   <h1>
     This is authored by <a v-bind:href="author"><span>data</span></a>
   </h1>
+
+  <button
+    class="border p-5 bg-blue-100 hover:cursor-pointer disabled:cursor-not-allowed disabled:bg-gray-400"
+    :disabled="isBtnDisabled"
+  >
+    Click
+  </button>
 </template>
 
 <style scoped></style>
->
