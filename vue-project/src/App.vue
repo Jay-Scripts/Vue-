b8e3ba35120f1c8f8f7f0ad8a5c7429bfc19c593
Jay-Scripts <corneliogatbontonjr21@gmail.com>
2026-07-09 15:41:29 +0800

    Attribute Binding

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -3,41 +3,16 @@ import { ref } from "vue";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import Test from "@/components/pages/Test.vue";
-const stringInterpolate = ref("long long string");
-const year = ref("2021");
-year.value = "2023";
-// const products = [
-//   {
-//     id: 1,
-//     name: "Wireless Headphones",
-//     price: "$89.99",
-//     image: "https://placehold.co/400x300",
-//   },
-//   {
-//     id: 2,
-//     name: "Smart Watch",
-//     price: "$149.99",
-//     image: "https://placehold.co/400x300",
-//   },
-//   {
-//     id: 3,
-//     name: "Mechanical Keyboard",
-//     price: "$99.99",
-//     image: "https://placehold.co/400x300",
-//   },
-//   {
-//     id: 4,
-//     name: "Gaming Mouse",
-//     price: "$49.99",
-//     image: "https://placehold.co/400x300",
-//   },
-// ];
+
+const author = ref("https://www.facebook.com/");
 </script>
 
 <template>
   <Test />
-  <h1>{{ stringInterpolate }} is interporated</h1>
-  <p>{{ year }}</p>
+
+  <h1>
+    This is authored by <a v-bind:href="author"><span>data</span></a>
+  </h1>
 </template>
 
 <style scoped></style>
