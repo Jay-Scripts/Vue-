dd1163676874034f1acde0838d5ee8e6b4ad919e
Jay-Scripts <corneliogatbontonjr21@gmail.com>
2026-07-09 15:32:39 +0800

    string interpolation

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import Test from "@/components/pages/Test.vue";
-
+const stringInterpolate = ref("long long string");
+const year = ref("2021");
+year.value = "2023";
 // const products = [
 //   {
 //     id: 1,
@@ -33,6 +36,9 @@ import Test from "@/components/pages/Test.vue";
 
 <template>
   <Test />
+  <h1>{{ stringInterpolate }} is interporated</h1>
+  <p>{{ year }}</p>
 </template>
 
 <style scoped></style>
+>
